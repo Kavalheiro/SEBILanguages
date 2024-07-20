a271970913378f2ccea4491674b10251a230e30f
Kavalheiro <octavio.kavalheiro@gmail.com>
2024-07-19 22:20:06 -0300

    inicial

---
+++ src/main/java/com/octavio/sebi_languages/SebiLanguagesApplication.java
@@ -0,0 +1,13 @@
+package com.octavio.sebi_languages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SebiLanguagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SebiLanguagesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/octavio/sebi_languages/SebiLanguagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.octavio.sebi_languages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SebiLanguagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
